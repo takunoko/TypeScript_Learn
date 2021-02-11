3c04ee6dfe4897c7088d8aa292573af8b8bab715
Takunoko <takunoko@gmail.com>
2021-02-11 15:04:55 +0900

    typescriptでjestテスト(ts-jest利用)をした

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    moduleFileExtensions: ["ts", "js"],
+    transform: {
+        "^.+\\.ts$": "ts-jest",
+    },
+    globals: {
+        "ts-jest": {
+            tsConfig: "tsconfig.json",
+        },
+    },
+    testMatch: ["**/tests/**/*.test.ts"],
+    moduleNameMapper: {
+        "^@/(.+)$": "<rootDir>/ts/$1",
+    },
+};
